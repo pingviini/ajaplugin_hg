4bd994cee262d3cd9efb941e8abd89c522f96438
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2014-01-03 15:44:39 +0200

    Refactor & fix pull/update logic.

--- src/ajaplugin_hg/mercurial.py
+++ src/ajaplugin_hg/mercurial.py
@@ -2,22 +2,24 @@ import logging
 import subprocess
 import shlex
 
-from path import path
 from aja.vcs.base import VcsBase
 
 
 class Mercurial(VcsBase):
     """Aja Mercurial support."""
 
-    def pull(self, repository_uri, destination, **kwargs):
-        logging.info("Pulling repository {uri} to {path}".format(
-            uri=repository_uri, path=destination))
-        with path(destination):
-            cmd = "hg pull {uri}".format(uri=repository_uri)
-            subprocess.check_call(shlex.split(cmd))
+    def pull(self, repository_uri):
+        logging.info("Pulling repository {uri}.".format(uri=repository_uri))
+        cmd = "hg clone {uri}".format(uri=repository_uri)
+        self.run(cmd)
 
-    def update(self, repository):
+    def update(self):
         logging.info("Updating repository {path}".format(path=repository))
-        with path(repository):
-            cmd = "hg update"
+        cmd = "hg pull; hg update"
+        self.run(cmd)
+
+    def run(self, cmd):
+        try:
             subprocess.check_call(shlex.split(cmd))
+        except subprocess.CalledProcessError as e:
+            logging.error(str(e))
